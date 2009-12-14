3b7ed1053018615983f9b1b3e9991bb002e24a6a
Brett Carswell <brett.carswell@gmail.com>
2009-12-14 21:30:48 +1000

    additional tests for encoding ints implemented encoding strings

--- Pomelo/src/main/scala/org/pomelo/message/MessageEncoder.scala
+++ Pomelo/src/main/scala/org/pomelo/message/MessageEncoder.scala
@@ -12,13 +12,16 @@ import scala.collection.mutable.ArrayBuffer
 class MessageEncoder {
     val buffer = new ArrayBuffer[byte]
 
-    def append(data : Any) = {
+    def append(data : Any) : Unit = {
         data match {
             case i: Int =>
                 buffer += (i >> 24).toByte
                 buffer += (i >> 16).toByte
                 buffer += (i >> 8).toByte
                 buffer += i.toByte
+            case s: String =>
+                append(s.length)
+                buffer ++= s.map( c => c.toByte )
         }
     }
 
--- Pomelo/src/test/scala/org/pomelotest/message/MessageEncoderSpec.scala
+++ Pomelo/src/test/scala/org/pomelotest/message/MessageEncoderSpec.scala
@@ -25,4 +25,69 @@ class MessageEncoderSpec extends Spec with ShouldMatchers {
             encoder.buffer(3) should equal (0)
         }
     }
+
+    val testDataA = 0x01234567
+    val testDataB = 0xdeadbeef
+
+    describe("encoding a single integer : " + testDataA) {
+        val encoder = new MessageEncoder
+        encoder.append(testDataA)
+
+        it ("should result in a buffer of length 4") {
+            encoder.buffer.length should equal (4)
+        }
+
+        it ("should contain the correct byte sequence") {
+            encoder.buffer(0) should equal (0x01)
+            encoder.buffer(1) should equal (0x23)
+            encoder.buffer(2) should equal (0x45)
+            encoder.buffer(3) should equal (0x67)
+        }
+    }
+
+    describe("encoding a two consecutive integers : " + testDataA + "," + testDataB) {
+        val encoder = new MessageEncoder
+        encoder.append(testDataA)
+        encoder.append(testDataB)
+
+        it ("should result in a buffer of length 8") {
+            encoder.buffer.length should equal (8)
+        }
+
+        it ("should contain the correct byte sequence") {
+            encoder.buffer(0) should equal (0x01.toByte)
+            encoder.buffer(1) should equal (0x23.toByte)
+            encoder.buffer(2) should equal (0x45.toByte)
+            encoder.buffer(3) should equal (0x67.toByte)
+            encoder.buffer(4) should equal (0xde.toByte)
+            encoder.buffer(5) should equal (0xad.toByte)
+            encoder.buffer(6) should equal (0xbe.toByte)
+            encoder.buffer(7) should equal (0xef.toByte)
+        }
+    }
+
+    val string = "test"
+
+    describe("encoding the string '" + string + "'") {
+        val expectedLength = 4 + string.length
+        val encoder = new MessageEncoder
+        encoder.append(string)
+
+        it ("should result in a buffer of length " + expectedLength) {
+            encoder.buffer.length should equal (expectedLength)
+        }
+
+        it ("should result in a buffer with the correct byte sequence") {
+            encoder.buffer(0) should equal (0)
+            encoder.buffer(1) should equal (0)
+            encoder.buffer(2) should equal (0)
+            encoder.buffer(3) should equal (4)
+            encoder.buffer(4) should equal ('t')
+            encoder.buffer(5) should equal ('e')
+            encoder.buffer(6) should equal ('s')
+            encoder.buffer(7) should equal ('t')
+        }
+    }
+
+
 }
