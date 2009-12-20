5b71f0f11666ea31c13c012c127fd748af58424f
Brett Carswell <brett.carswell@gmail.com>
2009-12-20 17:15:50 +1000

    Added documentation for Settings object

--- Pomelo/src/main/scala/org/pomelo/server/Settings.scala
+++ Pomelo/src/main/scala/org/pomelo/server/Settings.scala
@@ -1,15 +1,28 @@
-/*
- * settings.scala
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
- */
-
 package org.pomelo.server
 
+/**
+ * represents the settings of the xbmsp server
+ */
 object Settings {
+
+    /**
+     * The port that the server is listening on. Defaults to 4040
+     */
     var PORT : String = "4040"
+
+    /**
+     * The IP address for the server. An empty string indicates the IP address
+     * for the machine the server is running on
+     */
     var ADDRESS : String = ""
-    var VERSION : String = "1.0"
+
+    /**
+     *Server version string. Supporting v1.0 of the protocol
+     */
+    var VERSION : String = "XBMSP-1.0 1.0 Pomelo Server\n"
+
+    /**
+     * Server comment string
+     */
     var COMMENT : String = ""
 }
