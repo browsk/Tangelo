c14ba4128d2e1d2aef4117a5b4da4b1fb203a09d
Brett Carswell <brett.carswell@gmail.com>
2009-12-21 13:37:06 +1000

    Message decoder, currently can decode ints and strings only

---
+++ Pomelo/src/main/scala/org/pomelo/message/MessageDecoder.scala
@@ -0,0 +1,26 @@
+package org.pomelo.message
+
+import scala.collection.mutable.Buffer
+import scala.StringBuilder
+
+class MessageDecoder(data : Buffer[Byte]) {
+    val buffer = data
+
+    val it : Iterator[Byte] = data.elements
+
+    def getString() : String = {
+        val stringLength = getInt()
+        val string = new StringBuilder
+        it.take(stringLength).foreach( c => string.append(c.asInstanceOf[Char]) )
+
+        string.toString
+    }
+
+    def getInt() : Int = {
+        it.take(4).foldLeft(0)(
+            (result, current) =>
+                (result << 8) + (current.asInstanceOf[Short] & 0x00ff)
+        )
+    }
+
+}
---
+++ Pomelo/src/test/scala/org/pomelotest/message/MessageDecoderSpec.scala
@@ -0,0 +1,76 @@
+package org.pomelotest.message
+
+import org.pomelo.message.MessageDecoder
+import scala.collection.mutable.ArrayBuffer
+import org.scalatest.Spec
+import org.scalatest.matchers.ShouldMatchers
+
+class MessageDecoderSpec extends Spec with ShouldMatchers {
+
+    describe("A message decoder") {
+
+        describe("when initialised with a buffer containing a single int") {
+            val data : List[Byte] = List(0x00, 0x00, 0x12, (0xfe).asInstanceOf[Byte])
+            val buffer = new ArrayBuffer[Byte]
+            buffer.appendAll(data)
+
+            val decoder = new MessageDecoder(buffer)
+
+            it ("should contain an int of value 4862") {
+                val value = decoder.getInt()
+
+                value should equal (4862)
+            }
+        }
+
+        describe("when initialised with a buffer containing a consecutive ints ") {
+            val data : List[Byte] = List(0x00, 0x00, 0x12, (0xfe).asInstanceOf[Byte], (0x80).asInstanceOf[Byte], 0x00, 0x00, 0x01)
+            val buffer = new ArrayBuffer[Byte]
+            buffer.appendAll(data)
+
+            val decoder = new MessageDecoder(buffer)
+
+            it ("should contain an int of value 4862 followed by an int of value -2147483647") {
+                decoder.getInt() should equal (4862)
+                decoder.getInt() should equal (-2147483647)
+            }
+        }
+
+        describe("when initialised with a string 'hello'") {
+            val data : List[Byte] = List(0x00, 0x00, 0x00, 0x05, 'h', 'e', 'l', 'l', 'o')
+            val buffer = new ArrayBuffer[Byte]
+            buffer.appendAll(data)
+
+            val decoder = new MessageDecoder(buffer)
+
+            it ("should decode the string 'hello'") {
+                decoder.getString() should equal ("hello")
+            }
+        }
+
+        describe("when initialised with an empty string") {
+            val data : List[Byte] = List(0x00, 0x00, 0x00, 0x00)
+            val buffer = new ArrayBuffer[Byte]
+            buffer.appendAll(data)
+
+            val decoder = new MessageDecoder(buffer)
+
+            it ("should decode the string \"\"") {
+                decoder.getString() should equal ("")
+            }
+        }
+
+        describe("when initialised with a string 'hello' followed by the int 123") {
+            val data : List[Byte] = List(0x00, 0x00, 0x00, 0x05, 'h', 'e', 'l', 'l', 'o', 0x00, 0x00, 0x00, 123)
+            val buffer = new ArrayBuffer[Byte]
+            buffer.appendAll(data)
+
+            val decoder = new MessageDecoder(buffer)
+
+            it ("should decode the string 'hello' followed by 123") {
+                decoder.getString() should equal ("hello")
+                decoder.getInt() should equal (123)
+            }
+        }
+    }
+}
