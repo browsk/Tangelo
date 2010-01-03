24c8543263ec65917c3577605ce0f9d83d30e20f
Brett Carswell <brett.carswell@gmail.com>
2010-01-03 13:37:51 +1000

    Classes to write messages to a common interface

---
+++ Pomelo/src/main/scala/org/pomelo/io/StreamMessageWriter.scala
@@ -0,0 +1,30 @@
+/**
+ *   Copyright 2009 Brett Carswell
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ *   TcpMessageWriter.scala
+ *
+ */
+
+package org.pomelo.io
+
+import java.io.OutputStream
+import message.Message
+
+class StreamMessageWriter(stream: OutputStream) extends MessageWriter {
+    def write(message: Message) = {
+        val data = message.encode().toArray[Byte]
+        stream.write(data)
+    }
+}
---
+++ Pomelo/src/main/scala/org/pomelo/io/UdpMessageWriter.scala
@@ -0,0 +1,36 @@
+/**
+ *   Copyright 2009 Brett Carswell
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ *   UdpMessageWriter.scala
+ *
+ */
+
+package org.pomelo.io
+
+import org.pomelo.message.Message
+
+import java.net._
+
+object UdpMessageWriter {
+    private val socket = new DatagramSocket()
+}
+
+class UdpMessageWriter(destination: SocketAddress) extends MessageWriter {
+    def write(message: Message) = {
+        val data = message.encode().toArray[Byte]
+        val packet = new DatagramPacket(data, data.length, destination)
+        UdpMessageWriter.socket.send(packet)
+    }
+}
